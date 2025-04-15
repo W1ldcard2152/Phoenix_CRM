248ac5c2863f2b96aa8e54e1cec8fd31dd5a8694
W1ldcard2152 <gmoore2@gmail.com>
2025-04-15 09:42:30 -0400

    Fixed development server connection

--- src/server/routes/authRoutes.js
+++ src/server/routes/authRoutes.js
@@ -1,141 +1,33 @@
-const User = require('../models/User');
-const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
+const express = require('express');
+const authController = require('../controllers/authController');
+const userController = require('../controllers/userController');
+const router = express.Router();
 
-// Filter object to only allow specific fields
-const filterObj = (obj, ...allowedFields) => {
-  const newObj = {};
-  Object.keys(obj).forEach(el => {
-    if (allowedFields.includes(el)) newObj[el] = obj[el];
-  });
-  return newObj;
-};
+// Auth routes
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+router.get('/logout', authController.logout);
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
 
-// Get all users
-exports.getAllUsers = catchAsync(async (req, res, next) => {
-  const users = await User.find();
-  
-  res.status(200).json({
-    status: 'success',
-    results: users.length,
-    data: {
-      users
-    }
-  });
-});
+// Protected routes (require authentication)
+router.use(authController.protect);
 
-// Get current user
-exports.getMe = (req, res, next) => {
-  req.params.id = req.user.id;
-  next();
-};
+router.patch('/updateMyPassword', authController.updatePassword);
+router.get('/me', userController.getMe, userController.getUser);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
 
-// Update current user data (not password)
-exports.updateMe = catchAsync(async (req, res, next) => {
-  // 1) Create error if user tries to update password
-  if (req.body.password || req.body.passwordConfirm) {
-    return next(
-      new AppError(
-        'This route is not for password updates. Please use /updateMyPassword.',
-        400
-      )
-    );
-  }
-  
-  // 2) Filter unwanted fields that are not allowed to be updated
-  const filteredBody = filterObj(req.body, 'name', 'email');
-  
-  // 3) Update user document
-  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
-    new: true,
-    runValidators: true
-  });
-  
-  res.status(200).json({
-    status: 'success',
-    data: {
-      user: updatedUser
-    }
-  });
-});
+// Admin only routes
+router.use(authController.restrictTo('admin'));
 
-// Deactivate current user (soft delete)
-exports.deleteMe = catchAsync(async (req, res, next) => {
-  await User.findByIdAndUpdate(req.user.id, { active: false });
-  
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
-});
+router.route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
 
-// Get user by ID
-exports.getUser = catchAsync(async (req, res, next) => {
-  const user = await User.findById(req.params.id);
-  
-  if (!user) {
-    return next(new AppError('No user found with that ID', 404));
-  }
-  
-  res.status(200).json({
-    status: 'success',
-    data: {
-      user
-    }
-  });
-});
+router.route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
-// Create new user (admin only)
-exports.createUser = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
-  
-  res.status(201).json({
-    status: 'success',
-    data: {
-      user: newUser
-    }
-  });
-});
-
-// Update user (admin only)
-exports.updateUser = catchAsync(async (req, res, next) => {
-  // Don't allow password updates through this route
-  if (req.body.password || req.body.passwordConfirm) {
-    return next(
-      new AppError(
-        'This route is not for password updates. Please use /resetPassword.',
-        400
-      )
-    );
-  }
-  
-  const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true
-  });
-  
-  if (!user) {
-    return next(new AppError('No user found with that ID', 404));
-  }
-  
-  res.status(200).json({
-    status: 'success',
-    data: {
-      user
-    }
-  });
-});
-
-// Delete user (admin only - hard delete)
-exports.deleteUser = catchAsync(async (req, res, next) => {
-  const user = await User.findByIdAndDelete(req.params.id);
-  
-  if (!user) {
-    return next(new AppError('No user found with that ID', 404));
-  }
-  
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
-});
+module.exports = router;
